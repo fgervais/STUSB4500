a39cf74a6623c804015a619557c3604bb68a4933
Francois Gervais <francoisgervais@gmail.com>
2019-04-08 17:14:59 -0400

    Add the register inner class

--- Contrib/python_i2c_config/stusb.py
+++ Contrib/python_i2c_config/stusb.py
@@ -1,5 +1,7 @@
 class STUSB4500:
-    LAST_REGISTER_ADDR = 0x94
+
+    class Register:
+        LAST = 0x94
 
 
     def __init__(self, i2c_interface, i2c_address):
@@ -9,12 +11,12 @@ class STUSB4500:
     def __getitem__(self, key):
         if isinstance(key, slice):
             return [self.i2c_interface.read_byte_data(self.i2c_address, i)
-                for i in range(*key.indices(STUSB4500.LAST_REGISTER_ADDR))]
+                for i in range(*key.indices(STUSB4500.Register.LAST))]
         else:
             return self.i2c_interface.read_byte_data(self.i2c_address, key)
 
     def __setitem__(self, key, value):
-        if key > STUSB4500.LAST_REGISTER_ADDR:
+        if key > STUSB4500.Register.LAST:
             raise IndexError("list index out of range")
 
         self.i2c_interface.write_byte_data(self.i2c_address, key, value)
