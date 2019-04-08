70aaac10bb6bd89e5a8367511e4ae28db202d2f0
Francois Gervais <francoisgervais@gmail.com>
2019-04-08 17:15:36 -0400

    Add the device_id property

--- Contrib/python_i2c_config/stusb.py
+++ Contrib/python_i2c_config/stusb.py
@@ -1,6 +1,8 @@
 class STUSB4500:
 
     class Register:
+        DEVICE_ID = 0x2F
+
         LAST = 0x94
 
 
@@ -20,3 +22,7 @@ class STUSB4500:
             raise IndexError("list index out of range")
 
         self.i2c_interface.write_byte_data(self.i2c_address, key, value)
+
+    @property
+    def device_id(self):
+        self[STUSB4500.Register.DEVICE_ID]
